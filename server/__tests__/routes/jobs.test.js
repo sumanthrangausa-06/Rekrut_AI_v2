21766a39b518525f20b7124a1986aba03fe1a57d
Rekrut AI CTO <cto@rekrutai.co>
2026-06-13 02:47:34 +0800

    fix: correct test import paths and update test setup

--- server/__tests__/routes/jobs.test.js
+++ server/__tests__/routes/jobs.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('../../server')
+const app = require('../../../server')
 
 describe('Job Search API', () => {
   describe('GET /api/jobs', () => {
