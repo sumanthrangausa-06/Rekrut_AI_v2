6e69b9742b6126b31ce29ba9aa7eaf8d76f69fca
Rekrut AI CTO <cto@rekrutai.co>
2026-07-06 05:35:10 +0800

    chore(dev): cherry-pick d5719b1 CORS and auth rate limit fixes

--- server.js
+++ server.js
@@ -264,6 +264,8 @@ const ALLOWED_ORIGINS = [
 	'https://rekrutai-dev.onrender.com',
 	'https://rekrutai.co',
 	'http://localhost:5173',
+	'http://localhost:3000',
+	'http://localhost:3001',
 ];
 
 app.use(
